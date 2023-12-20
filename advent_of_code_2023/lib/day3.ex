b87ee5a7495e7637d626bd1dfde05feb3ed01c1c
JamieVaughn <wxv6@case.edu>
2023-12-20 18:18:30 -0500

    wip: create fns to extract indices of symbols and numbers and return the numbers

---
+++ advent_of_code_2023/lib/day3.ex
@@ -0,0 +1,66 @@
+defmodule Day3 do
+  
+  def getWidth (input) do
+    input
+      |>String.split("\n")
+      |>List.first
+      |>String.length
+  end
+  
+  def getSymbolIdxList (input) do
+    _width = Day3.getWidth(input)
+    str = String.split(input) |> to_string
+    Regex.scan(~r/[*|#|$|+]/, str, return: :index)
+      |> List.flatten
+      |> Enum.map(fn (x) -> elem(x, 0) end)
+      # |> IO.inspect
+      
+  end
+  
+  def getPartNumbersIdx(input) do
+    _width = Day3.getWidth(input)
+    str = String.split(input) |> to_string
+    Regex.scan(~r/\d+/, str, return: :index)
+    |> List.flatten
+    |> Enum.map(fn (x) -> x end)
+    # |> IO.inspect
+  end
+  
+  def isAdjacent(symb, num, width) do
+    symb+1 == num ||
+    symb-1 == num || 
+    symb+width == num || 
+    symb-width == num ||
+    symb-width-1 == num ||
+    symb-width+1 == num ||
+    symb+width-1 == num ||
+    symb+width+1 == num 
+  end
+  
+  def isPartNumberAdjacent(input) do
+    width = Day3.getWidth(input)
+    str = String.split(input) |> to_string
+    Day3.getPartNumbersIdx(input)
+    |> Enum.filter(fn (num) -> 
+      Day3.getSymbolIdxList(input)
+      |> Enum.map(fn (symb) -> 
+        Enum.any?(1..elem(num, 1), fn (idx) -> 
+          isAdjacent(symb, elem(num, 0) + idx, width)
+        end)
+      end)   
+    end)
+    |> IO.inspect
+    |> Enum.map(fn y -> 
+        Enum.map(1..elem(y, 1), fn z -> 
+          Enum.at(String.graphemes(str), (elem(y, 0) - 1 + z)) end)
+     end)
+    |> Enum.map(fn x -> 
+        Enum.join(x, "")
+    end)
+    |> Enum.map(fn x -> 
+        String.to_integer(x)
+    end)
+    |> IO.inspect
+    
+  end
+end
